8970bccb29cd732230f81f944bf7daa805156fb5
gguerra92 <77221428+gguerra92@users.noreply.github.com>
2026-08-22 23:07:19 -0600

    Forzar scripts nuevos de integracion financiera

--- index.php
+++ index.php
@@ -195,8 +195,8 @@ $email = htmlspecialchars((string) ($user['email'] ?? ''), ENT_QUOTES, 'UTF-8');
     </details>
   </footer>
 
-  <script src="./app.js?v=20260822-fin-int-3"></script>
-  <script src="./solicitud-integracion.js?v=20260822-fin-int-3"></script>
-  <script src="./solicitud-precarga-fallback.js?v=20260822-fin-int-3"></script>
+  <script src="./app.js?v=20260822-fin-int-4"></script>
+  <script src="./solicitud-integracion.js?v=20260822-fin-int-4"></script>
+  <script src="./solicitud-precarga-fallback.js?v=20260822-fin-int-4"></script>
 </body>
 </html>
